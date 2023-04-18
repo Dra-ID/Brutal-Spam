3e121712c9913e57428bf5c0b36abb8f15e4dcef
vindra XD <104926748+Dra-ID@users.noreply.github.com>
2023-04-18 22:01:22 +0700

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,12 @@
+pkg install python
+pkg install python2
+pkg install nano
+pkg install git
+pkg install inquirer
+pip install inquirer
+pip install requests
+pip install mechanize
+
+sleep 3
+
+python run.py
